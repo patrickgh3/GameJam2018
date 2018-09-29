2e0c403d095c17633647ed362815bd8b2ec8e824
Patrickgh3 <patrickjtraynor@gmail.com>
2018-09-29 08:42:46 -0700

    Player collision with walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void Start() {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate() {
+        float moveSpeed = 300f * Time.deltaTime;
+        Vector3 toMove = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, Input.GetAxisRaw("Vertical") * moveSpeed);
+        toMove = new Vector3(Mathf.Round(toMove.x), Mathf.Round(toMove.y));
+
+        transform.position += toMove;
+
+        Vector2 size = new Vector2(100, 100);
+        int wallsLayer = 1 << 9;
+
+        if (Physics2D.OverlapBox(transform.position, size, 0, wallsLayer)) {
+            transform.position -= toMove;
+
+            for (int i = 0; i < Mathf.Abs(toMove.x); i++) {
+                transform.position += new Vector3(Mathf.Sign(toMove.x), 0);
+                if (Physics2D.OverlapBox(transform.position, size, 0, wallsLayer)) {
+                    transform.position -= new Vector3(Mathf.Sign(toMove.x), 0);
+                    break;
+                }
+            }
+
+            for (int i = 0; i < Mathf.Abs(toMove.y); i++) {
+                transform.position += new Vector3(0, Mathf.Sign(toMove.y));
+                if (Physics2D.OverlapBox(transform.position, size, 0, wallsLayer)) {
+                    transform.position -= new Vector3(0, Mathf.Sign(toMove.y));
+                    break;
+                }
+            }
+        }
+    }
 }
